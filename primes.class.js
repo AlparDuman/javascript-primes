99473b6149218fd80d38ca39045449c740eb6095
AlparDuman <>
2025-08-03 02:33:27 +0200

    add execution ms timer for tests

--- primes.class.js
+++ primes.class.js
@@ -62,19 +62,24 @@ class primes {
         const end = start + range;
 
         let resultsTD = [];
+        console.time('exec');
         for (let i = start; i < end; i++)
             resultsTD.push(this.isPrimeTrialDivision(i));
+        console.timeEnd('exec');
+        console.log('Test isPrimeTrialDivision()', resultsTD);
 
         let resultsSE = [];
+        console.time('exec');
         for (let i = start; i < end; i++)
             resultsSE.push(this.isPrimeSieveEratosthenes(i));
+        console.timeEnd('exec');
+        console.log('Test isPrimeSieveEratosthenes()', resultsSE);
 
         let resultsBS = [];
+        console.time('exec');
         for (let i = start; i < end; i++)
             resultsBS.push(this.isPrimeBucketSieve(i));
-
-        console.log('Test isPrimeTrialDivision()', resultsTD);
-        console.log('Test isPrimeSieveEratosthenes()', resultsSE);
+        console.timeEnd('exec');
         console.log('Test isPrimeBucketSieve()', resultsBS);
 
         for (let i = start; i < end; i++)
@@ -113,12 +118,19 @@ class primes {
     selfTestCountPrimes(range = 20, start = 0) {
         console.log('Self test count primes');
 
+        console.time('exec');
         let resultsTD = this.countPrimesTrialDivision(range, start);
-        let resultsSE = this.countPrimesSieveEratosthenes(range, start);
-        let resultsBS = this.countPrimesBucketSieve(range, start);
-
+        console.timeEnd('exec');
         console.log('Test countPrimesTrialDivision()', resultsTD);
+
+        console.time('exec');
+        let resultsSE = this.countPrimesSieveEratosthenes(range, start);
+        console.timeEnd('exec');
         console.log('Test countPrimesSieveEratosthenes()', resultsSE);
+
+        console.time('exec');
+        let resultsBS = this.countPrimesBucketSieve(range, start);
+        console.timeEnd('exec');
         console.log('Test countPrimesBucketSieve()', resultsBS);
 
         for (let i = start; i < range; i++)
@@ -173,12 +185,19 @@ class primes {
     selfTestGetPrimes(range = 20, start = 0) {
         console.log('Self test get primes');
 
+        console.time('exec');
         let resultsTD = this.getPrimesTrialDivision(range, start);
-        let resultsSE = this.getPrimesSieveEratosthenes(range, start);
-        let resultsBS = this.getPrimesBucketSieve(range, start);
-
+        console.timeEnd('exec');
         console.log('Test getPrimesTrialDivision()', resultsTD);
+
+        console.time('exec');
+        let resultsSE = this.getPrimesSieveEratosthenes(range, start);
+        console.timeEnd('exec');
         console.log('Test getPrimesSieveEratosthenes()', resultsSE);
+
+        console.time('exec');
+        let resultsBS = this.getPrimesBucketSieve(range, start);
+        console.timeEnd('exec');
         console.log('Test getPrimesBucketSieve()', resultsBS);
 
         for (let i = start; i < range; i++)
